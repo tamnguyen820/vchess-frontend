5ab8c49c071f0b4e9fe6f462aa3ee5ea2344488a
Tam Nguyen <tam.nguyen8232@gmail.com>
2023-12-24 16:12:44 -0500

    Change img destination

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="view">
     <h1>
-      <img
-        src="/public/assets/images/icon/search-glass.svg"
-        alt="Not found"
-      />Not Found
+      <img src="/src/assets/images/icon/search-glass.svg" alt="Not found" />Not Found
     </h1>
     <p>
       Oops, we couldn't find that page. Let's go
@@ -26,9 +23,11 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   img {
     height: 3.5rem;
   }
+
   h1 {
     font-size: 5rem;
     margin-top: -3rem;
